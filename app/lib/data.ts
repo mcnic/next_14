e8b6554c533e2e2a7a4095e0f9dff65246d33967
max <mcnic@users.noreply.github.com>
2024-05-25 19:03:35 -0700

    feat: add Card

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -12,6 +12,7 @@ import {
 } from './definitions';
 import { formatCurrency } from './utils';
 import { InvoicesMobileSkeleton } from '../ui/skeletons';
+import { Prisma } from '@prisma/client';
 
 export async function fetchRevenue() {
   // Add noStore() here to prevent the response from being cached.
@@ -51,7 +52,7 @@ export async function fetchLatestInvoices() {
       },
       relationLoadStrategy: 'join',
       orderBy: [{ date: 'desc' }],
-      take: 10,
+      take: 5,
     });
 
     const latestInvoices = data.map(({ id, amount, Customer }) => ({
@@ -68,40 +69,44 @@ export async function fetchLatestInvoices() {
   }
 }
 
-// export async function fetchCardData() {
-//   try {
-//     // You can probably combine these into a single SQL query
-//     // However, we are intentionally splitting them to demonstrate
-//     // how to initialize multiple queries in parallel with JS.
-//     const invoiceCountPromise = sql`SELECT COUNT(*) FROM invoices`;
-//     const customerCountPromise = sql`SELECT COUNT(*) FROM customers`;
-//     const invoiceStatusPromise = sql`SELECT
-//          SUM(CASE WHEN status = 'paid' THEN amount ELSE 0 END) AS "paid",
-//          SUM(CASE WHEN status = 'pending' THEN amount ELSE 0 END) AS "pending"
-//          FROM invoices`;
+export async function fetchCardData() {
+  try {
+    const query = Prisma.raw(`SELECT
+       SUM(CASE WHEN status = 'PAID' THEN amount ELSE 0 END) AS "paid",
+       SUM(CASE WHEN status = 'pending' THEN amount ELSE 0 END) AS "pending"
+       FROM invoices`);
 
-//     const data = await Promise.all([
-//       invoiceCountPromise,
-//       customerCountPromise,
-//       invoiceStatusPromise,
-//     ]);
+    const data = await Promise.all([
+      prisma.invoice.count(),
+      prisma.customer.count(),
+      prisma.invoice.findMany({
+        where: {
+          status: 'PAID',
+        },
+      }),
+      prisma.invoice.findMany({
+        where: {
+          status: 'PENDING',
+        },
+      }),
+    ]);
 
-//     const numberOfInvoices = Number(data[0].rows[0].count ?? '0');
-//     const numberOfCustomers = Number(data[1].rows[0].count ?? '0');
-//     const totalPaidInvoices = formatCurrency(data[2].rows[0].paid ?? '0');
-//     const totalPendingInvoices = formatCurrency(data[2].rows[0].pending ?? '0');
+    const numberOfInvoices = Number(data[0] ?? '0');
+    const numberOfCustomers = Number(data[1] ?? '0');
+    const totalPaidInvoices = formatCurrency(data[2].length ?? '0');
+    const totalPendingInvoices = formatCurrency(data[3].length ?? '0');
 
-//     return {
-//       numberOfCustomers,
-//       numberOfInvoices,
-//       totalPaidInvoices,
-//       totalPendingInvoices,
-//     };
-//   } catch (error) {
-//     console.error('Database Error:', error);
-//     throw new Error('Failed to fetch card data.');
-//   }
-// }
+    return {
+      numberOfCustomers,
+      numberOfInvoices,
+      totalPaidInvoices,
+      totalPendingInvoices,
+    };
+  } catch (error) {
+    console.error('Database Error:', error);
+    throw new Error('Failed to fetch card data.');
+  }
+}
 
 // const ITEMS_PER_PAGE = 6;
 // export async function fetchFilteredInvoices(
